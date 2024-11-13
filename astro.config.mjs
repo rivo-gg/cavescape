bd36dec51beea99d04a52e6bc1d7ed8411c2e34a
Dominik Koch <dominik@koch-bautechnik.de>
2024-11-13 19:08:31 +0100

    fix: make standalone

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import nodejs from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: nodejs({ mode: "standalone" }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
