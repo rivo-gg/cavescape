ed0a87bdee5d90563e6820f82b1a259bc1d9bcbf
Diogo Paulos <itzframepvp@outlook.com>
2025-02-25 23:57:49 +0000

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import nodejs from '@astrojs/node'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   output: 'server',
-  adapter: nodejs({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [
     tailwind({
       applyBaseStyles: false
